1b3afae57c3b8d542c550abb64bb3f7749c28d4d
Cristiana Passos <cristiana.passos@bee-eng.pt>
2020-06-19 18:42:52 +0100

    first commit

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DataService } from '../data.service';
+@Component({
+  selector: 'search-component',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  searchForm: FormGroup;
+  countrySearch: string;
+  constructor(private fb: FormBuilder, public service: DataService) { }
+
+  ngOnInit() {
+
+    // add corresponding validators
+    this.searchForm = this.fb.group({
+      'searchCountry': [null]
+    });
+
+    // write a function that calls changeCountryName upon value change in the form
+  }
+
+  changeCountryName(e: any) {
+    this.service.changeCountryName(e.target.value);
+  }
+
+}
